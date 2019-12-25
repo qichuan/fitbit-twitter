677f73052a072c43592c95dae05fa63c69efecc2
ZHANG Qichuan <qichuan@zhangqichuan.com>
2019-12-25 15:34:28 +0800

    Add method for retweet

--- common/twitter_api.js
+++ common/twitter_api.js
@@ -5,6 +5,7 @@ const requestTokenUrl = "https://api.twitter.com/oauth/request_token";
 const accessTokenUrl = "https://api.twitter.com/oauth/access_token";
 const homeTimelineUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json";
 const likeTweetUrl = "https://api.twitter.com/1.1/favorites/create.json";
+const retweetUrl = "https://api.twitter.com/1.1/statuses/retweet/:id.json";
 
 function getRequestToken(newCallbackUrl, callback) {
     // Use this proxy to bypass the cross-origin restriction in the settings page
@@ -47,13 +48,29 @@ function getHomeTimeline(accessToken, accessTokenSecret, callback) {
 }
 
 function likeTweet(tweetIdToLike, accessToken, accessTokenSecret, callback) {
-    const url = likeTweetUrl+"?id="+tweetIdToLike;
+    const url = likeTweetUrl + "?id=" + tweetIdToLike;
+    fetch(url, {
+        method: "POST",
+        headers: {
+            Authorization: twitterOAuth
+                .getAuthorizationForProtectedResource('POST', likeTweetUrl, 
+                    accessToken, accessTokenSecret, {id: tweetIdToLike})
+        }
+    }).then(res => res.text())
+        .then(text => callback(text))
+        .catch(error => console.log('Error: ' + error.toLocaleString()));
+}
+
+function retweet(tweetIdToRetweet, accessToken, accessTokenSecret, callback) {
+    const url = retweetUrl.replace(':id', tweetIdToRetweet);
+    console.log(url);
     fetch(url, {
         method: "POST",
         headers: {
             Authorization: twitterOAuth
                 .getAuthorizationForProtectedResource('POST', url, 
-                    accessToken, accessTokenSecret)
+                    accessToken, accessTokenSecret),
+            'Content-Type': 'application/json'
         }
     }).then(res => res.text())
         .then(text => callback(text))
@@ -83,7 +100,7 @@ function processAccessTokenResultQueryText(queryText, callback) {
 }
 
 const twitterApi = {
-    getRequestToken, getAccessToken, getHomeTimeline, likeTweet
+    getRequestToken, getAccessToken, getHomeTimeline, likeTweet, retweet
 };
 
 export {twitterApi}
